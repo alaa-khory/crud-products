2758a6285e92a4ed8485f279df37149848147f78
alaa-khory <alaa.khory9418@gmail.com>
2024-10-24 13:05:57 +0400

    initial commit

---
+++ src/main/java/org/example/crud_products/SpringBootCrudProductsApplication.java
@@ -0,0 +1,14 @@
+package org.example.crud_products;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+
+public class SpringBootCrudProductsApplication {
+
+    public static void main(String args[]) {
+        SpringApplication.run(SpringBootCrudProductsApplication.class, args);
+    }
+
+}
---
+++ src/main/java/org/example/crud_products/controller/ProductsController.java
@@ -0,0 +1,17 @@
+package org.example.crud_products.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/products")
+public class ProductsController {
+
+    @GetMapping
+    public String getString(){
+        return "Test endpoint";
+    }
+
+
+}
